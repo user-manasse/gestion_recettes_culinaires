b36c34545515be71e021d3dea764cc6647c9d7d1
Manassé TOVONKERY <manasse.tovonkery@kosmopolead.com>
2024-11-19 11:37:58 +0300

    modif delete step

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,5 +2,5 @@ class Step < ApplicationRecord
   belongs_to :recipe
 
   validates :description, presence: true
-  validates :position, presence: true, numericality: { only_integer: true }
+  validates :position, presence: false, numericality: { only_integer: true }
 end
